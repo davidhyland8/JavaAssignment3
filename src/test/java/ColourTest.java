35159cdacc0c42cb639586e3ab8cadad03f07301
david <118363651@umail.ucc.ie>
2020-12-17 13:53:53 +0000

    Created test to check if add function works correctly

--- src/test/java/ColourTest.java
+++ src/test/java/ColourTest.java
@@ -9,4 +9,12 @@ class ColourTest {
         Colour colour2 = new Colour(200, 1, 30, "RGB");
         assertTrue(colour1.equalsColour(colour2));
     }
+
+    @Test
+    public void add(){
+        Colour colour3 = new Colour(20, 15, 30, "RGB");
+        Colour colour4 = new Colour(2, 40, 30, "RGB");
+        Colour colour5 = new Colour(22, 55, 60, "RGB");
+        assertTrue(colour5 == colour3.add(colour4));
+    }
 }
